9d896588e85485f50c3e5bc5a46c214e7eea8f8e
Vaishak <vaishakk@gmail.com>
2026-07-12 16:43:49 +0200

    modify api-main to make it testable - add rag_systen as a fastapi dependency

--- api/__init__.py
+++ api/__init__.py
@@ -1,10 +1,9 @@
 from .adapters import RAGAdapter, chunk2doc, doc2chunk
-from .api import answer_query, app, build_rag_system, get_rag_system, read_root
+from .api import app, build_rag_system, get_rag_system
 from .core import Document, InferQuery, InferenceResults, Query, SearchResults
 
 __all__ = [
     "RAGAdapter",
-    "answer_query",
     "app",
     "build_rag_system",
     "chunk2doc",
@@ -14,6 +13,5 @@ __all__ = [
     "InferQuery",
     "InferenceResults",
     "Query",
-    "read_root",
     "SearchResults",
 ]
--- api/api/main.py
+++ api/api/main.py
@@ -1,18 +1,21 @@
 import os
 from functools import lru_cache
-from fastapi import FastAPI
+from typing import Annotated
 
-from rag.rag.vectordb import ElasticsearchVectorDB
+from fastapi import FastAPI, Depends
+from requests import Session
+
+from rag import ElasticsearchVectorDB, DictDocumentRepository
 from rag.rag.embedding import OpenAIEmbeddingModel
 from rag.rag.chunkers import LlamaIndexChunker
 from rag.core.rag import RAG
-from rag.rag.markdown_extractors import DocumentStackFromMarkdownFolder, PlainTextExtractor
+from rag.rag.document_extractors.markdown_extractors import DocumentStackFromMarkdownFolder
 
-app = FastAPI()
+#app = FastAPI()
 
 def build_rag_system(index_name: str | None = None) -> RAG:
     # stack = DocumentStackFromPDFFolder('./docs', PyPDFExtractor())
-    stack = DocumentStackFromMarkdownFolder('./docs/Markdowns', PlainTextExtractor())
+    stack = DocumentStackFromMarkdownFolder('./docs/Markdowns', DictDocumentRepository())
     embed_model = OpenAIEmbeddingModel()
     chunker = LlamaIndexChunker()
     resolved_index_name = index_name or os.environ.get("ES_INDEX_NAME")
@@ -23,26 +26,30 @@ def build_rag_system(index_name: str | None = None) -> RAG:
             model=embed_model,
             chunker=chunker,
             index_name=resolved_index_name,
+            skip_indexing=True
         )
     )
-
-
 @lru_cache(maxsize=1)
 def get_rag_system() -> RAG:
     return build_rag_system()
+
+app = FastAPI()
+
+def __init__(self, rag_system: RAG):
+    super().__init__()
+    self.rag_system = rag_system
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-
 @app.get("/search/{query}")
-def answer_query(query: str | None = None):
+def answer_query(query: str, rag_system: Annotated[RAG, Depends(get_rag_system)]):
+    print(query)
     ans = rag_system.retrieve(query)
     return {"answer": ans, "q": query}
 
 if __name__ == "__main__":
-    # This block only runs if you do: python main.py
     rag_system = get_rag_system()
     import uvicorn
     uvicorn.run(app, host="127.0.0.1", port=8000)
